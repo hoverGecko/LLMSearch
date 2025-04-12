bf5f36af688472d5f863b0e42d65c1a2e8601d85
hoverGecko <u3593006@connect.hku.hk>
2025-04-13 07:21:55 +0800

    Add MaterialCommunity font to fix icons

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    MaterialCommunityIcons: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf'),
   });
 
   useEffect(() => {
