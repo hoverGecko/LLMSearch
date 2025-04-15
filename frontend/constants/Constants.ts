31e8349bb8be19a6bd460e3a102a6a734b279f5d
hoverGecko <u3593006@connect.hku.hk>
2025-04-15 08:20:48 +0800

    Fix template.yaml

--- frontend/constants/Constants.ts
+++ frontend/constants/Constants.ts
@@ -1,12 +1,6 @@
 export const backendUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
 export const apiKey = process.env.EXPO_PUBLIC_API_KEY;
-export const cognitoPoolId = process.env.EXPO_PUBLIC_COGNITO_POOL_ID!;
-export const cognitoClientId = process.env.EXPO_PUBLIC_COGNITO_CLIENT_ID!;
 
 if (!apiKey) {
     console.warn("API Key (EXPO_PUBLIC_API_KEY) is not set. Requests might fail if the backend requires it.");
-}
-
-if (!cognitoPoolId || !cognitoPoolId) {
-    throw new Error("Cognito Pool ID or Client ID is not set up in env.")
 }
